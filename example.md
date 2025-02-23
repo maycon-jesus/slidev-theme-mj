7b60fcc12106535772b9794274c3758e703e2b0c
Maycon Jesus <contato@mayconjesus.dev>
2025-02-23 17:46:52 -0300

    feat: layout image-left

--- example.md
+++ example.md
@@ -18,3 +18,12 @@ layout: section
 # Ola mundo
 
 teste de conteudo
+
+---
+layout: image-left
+image: https://cover.sli.dev
+---
+
+# Imagem na esquerda
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat varius tortor ac placerat. Donec eget bibendum nisl. Fusce quis hendrerit nunc, ut auctor mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin euismod, ligula id maximus rutrum, tellus turpis dignissim ipsum, finibus pharetra lacus odio sit amet justo. Curabitur ultricies orci sit amet ultrices ultricies. Mauris lobortis arcu quam, ut viverra lectus vehicula ac. Fusce blandit placerat orci, nec lacinia dui fermentum non. Nullam a mattis dui. Ut faucibus sapien nec placerat posuere. Phasellus elit ipsum, cursus non arcu sed, blandit consectetur elit.
---
+++ layouts/image-left.vue
@@ -0,0 +1,54 @@
+<script setup>
+import FooterLogo from "../components/FooterLogo.vue";
+
+const imagePath = $frontmatter.image;
+const imageWidth = $slidev.configs.canvasWidth*0.4
+</script>
+
+<template>
+  <div class="slidev-layout mj-layout-image-left h-full">
+    <div class="wrapper h-full">
+      <div class="left-bar"></div>
+      <img class="layout-img" :src="imagePath" :width="imageWidth" />
+      <div class="content">
+        <slot/>
+      </div>
+    </div>
+    <FooterLogo class="footerLogo" :logo-path="$slidev.themeConfigs.logoPath" v-if="$slidev.themeConfigs.logoPath"/>
+  </div>
+</template>
+
+<style scoped>
+.footerLogo{
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+.slidev-layout{
+  position: relative;
+
+  .wrapper{
+    display: flex;
+    flex-flow: row nowrap;
+
+    .left-bar{
+      height: 100%;
+      width: var(--mj-content-padding);
+      background: var(--mj-primary-secondary-gradient);
+      flex: none;
+    }
+
+    .layout-img{
+      object-fit: cover;
+      object-position: center center;
+      flex: none;
+    }
+
+    .content{
+      padding: 0 var(--mj-content-padding);
+      flex: 1;
+    }
+  }
+}
+</style>
